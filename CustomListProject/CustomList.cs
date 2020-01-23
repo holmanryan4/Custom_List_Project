9adbeeb9ef2675324ab4b4727a4babdfaa4420cc
unknown <holmanryan4@gmail.com>
2020-01-23 16:58:12 -0600

    i have finished creating test for the adding items to a list class. I will be moving on to removing items from a list

--- CustomListProject/CustomList.cs
+++ CustomListProject/CustomList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CustomListProject
 {
-    public class CustomList
+    public class CustomList<T>
     {
     }
 }
---
+++ CustomListTests/AddingToListTest.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListProject;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class AddingToListTest
+    {
+        [TestMethod]
+        public void AddingToList_AddingOneItem_ToTheList_AndMakingItShows_AsTheFirstItemInTheList()
+        {
+            //Arrange 
+            CustomList<int> custom = new CustomList<int>();
+            int actual;
+            int expected = 1;
+            // Act
+            custom.Add(5);
+            actual = custom.Count;
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void AddingToList_AddingAnItemToList_AndHaveingItShowUpInTheZeroindexSpace()
+        {
+            //Arrange
+            CustomList<int> custom = new CustomList<int>();
+            int actual;
+            int expected = 7;
+
+            //Act
+            custom.Add(7);
+            actual = custom[0];
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void AddingToList_AddingSecondItemToTheList_AndhavingTheitemShowInTheList_InSecondPlaceonList()
+        {
+            //Arrange
+            CustomList<int> custom = new CustomList<int>();
+            int firstNum = 12;
+            int secondNum = 4;
+            int actual;
+            int expected = 4;
+            //Act
+            custom.Add(firstNum);
+            custom.Add(secondNum);
+            actual = custom[1];
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void AddingToList_AddTheSameItemTwice_MakeSureBothItemsAreAddedToTheList()
+        {
+            //Arrange
+            CustomList<string> custom = new CustomList<string>();
+
+            string officeItem1 = "Desk";
+            string Officeitem2 = "Desk";
+            int actual;
+            string expected = "Desk";
+            //Act
+            custom.Add(officeItem1);
+            custom.Add(Officeitem2);
+            actual =  custom[1];
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void AddingToList_AddFiveItemsToTheList_MakeSureCompacityGrowsToEightInSize()
+        {
+          //Arrange
+            CustomList<int> customNumbers = new CustomList<int>();
+            int expected = 8;
+            int actual;
+
+
+          //Act
+            customNumbers.Add(6);
+            customNumbers.Add(4);
+            customNumbers.Add(12);
+            customNumbers.Add(44);
+            customNumbers.Add(56);
+            actual = customNumbers.Capacity;
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+        //test adding a second value and check to see if its at the 1st index
+        //add a second item and check to made sure the 0 index is still the same item  added
+        //add five items and check to see if the compacity has grown to 8 in size
+        // add the same thing twice and make sure its in the list twice
+    }
+}
