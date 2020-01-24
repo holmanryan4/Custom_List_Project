1874dce3e40707a47656a9065ce0aa19a02bde44
unknown <holmanryan4@gmail.com>
2020-01-24 12:08:19 -0600

    I have laid the foundation for the remove class and now I am working on writing the second test for this class

--- CustomListTests/RemovingFromListTest.cs
+++ CustomListTests/RemovingFromListTest.cs
@@ -8,8 +8,52 @@ namespace CustomListTests
     public class RemovingFromListTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CustomList_RemovingOneItemFromTheList()
         {
+            //Arrange
+            CustomList<int> customNumbers = new CustomList<int>();
+            int expected = 0;
+            int actual;
+
+            //Act
+            customNumbers.Add(4);
+            customNumbers.Remove(4);
+            actual = customNumbers.Count; 
+            //Assert
+            Assert.AreEqual(expected, actual);
         }
+        public void CustomList_RemovingTwoItemsFromTheList()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+        public void CustomList_RemoveOneOfTheDuplicatedItemsFromTheList_WithoutRemovingBothItems()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+        public void CustomList_WhenRemovingAnItemFromTheList_MakeSureTheOtherItemsInTheListMovesupinTheList()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+        public void CustomList_MakeSureItemsThatAreNotInTheList_CanNotBeRemoved()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+        public void CustomList_WhenRemovingFiveitemsFromTheList_makeSureTheCapacityDoesNotShrink()
+        {
+            //Arrange
+            //Act
+            //Assert
+        }
+
     }
+
+    
 }
